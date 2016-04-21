b9ff08705262fd7e2e6b9403152bc19ad62b788d
1457792186 <1457792186@qq.com>
2016-04-21 19:30:49 +0800

    JW:加载数据

---
+++ MostVisitors/MostVisitors/BottomView/JWBottomModel.h
@@ -0,0 +1,13 @@
+//
+//  JWBottomModel.h
+//  MostVisitors
+//
+//  Created by scjy on 16/4/21.
+//  Copyright © 2016年 蒋威. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JWBottomModel : NSObject
+
+@end
---
+++ MostVisitors/MostVisitors/BottomView/JWBottomModel.m
@@ -0,0 +1,13 @@
+//
+//  JWBottomModel.m
+//  MostVisitors
+//
+//  Created by scjy on 16/4/21.
+//  Copyright © 2016年 蒋威. All rights reserved.
+//
+
+#import "JWBottomModel.h"
+
+@implementation JWBottomModel
+
+@end
